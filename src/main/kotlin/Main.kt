2eb38baa967c6006e95837d8abe04f9217182a98
Rico Apon <rico.apon@gmail.com>
2025-08-02 17:58:06 +0200

    Create algorithm for generating levels

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,53 @@
 package org.example
 
+import org.example.grid.GridGenerator
+import org.example.grid.GridGeneratorWithPlayerAndBunnyIterator
+import org.example.level.Grid
+import org.example.solver.Solver
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
 fun main() {
-    println("Hello World!")
+    val generatedLevels = mutableListOf<Grid>()
+    val solver = Solver(100)
+
+    var levelsChecked = 0
+
+    while(generatedLevels.size < 1) {
+        val grid = generateGrid()
+        val solution = solver.bfsSolve(grid)
+        levelsChecked++
+
+        if (levelsChecked % 100 == 0) {
+            val time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+            println("[$time] Checked $levelsChecked levels, found ${generatedLevels.size}")
+        }
+
+        // If the grid is solvable, almost all positions are solvable. This might not always be the case. But to speed
+        // up the algorithm, we discard any grid for which we cannot find a solution for the first player/bunny position.
+        if (solution == null) {
+            iterator = null
+            continue
+        }
+
+        if (solution.size() > 30) {
+            generatedLevels.add(grid)
+        }
+    }
+
+    for (grid in generatedLevels) {
+        print(grid.toAsciiArt())
+        println()
+    }
+}
+
+
+var iterator: GridGeneratorWithPlayerAndBunnyIterator? = null
+private fun generateGrid(): Grid {
+    if (iterator == null || !iterator!!.hasNext()) {
+        val grid = GridGenerator().generate()
+        iterator = GridGeneratorWithPlayerAndBunnyIterator(grid, 14, 8)
+    }
+
+    return iterator!!.next()
 }
